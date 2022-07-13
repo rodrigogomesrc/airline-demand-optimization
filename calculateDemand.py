3c1dbf097cc504e5300f2e97d2f1f2a0d1a70349
rodrigogomesrc <rocha.rodrigogomes@gmail.com>
2022-07-13 16:00:59 -0300

    remove the need for an adjacent matrix

--- calculateDemand.py
+++ calculateDemand.py
@@ -1,41 +1,20 @@
 import sys
 
 filename = sys.argv[1]
-adjancy_matrix = []
+edges = []
 lines = 0
-matrix_filled = False
 
 third_values = []
 vertices = []
 header = ""
 
-def fill_matrix(no_fill):
-    if no_fill:
-        return
-    for i in range(lines):
-        line = [0 for i in range(lines)]
-        adjancy_matrix.append(line)
-
-
-def print_matrix():
-    x_indice = "n => " + str([i for i in range (lines)])
-    print(x_indice)
-    for i in range(lines):
-        print(str(i) + " => " + str(adjancy_matrix[i]))
-
-
-def extract_graph_edges(adjancy_matrix):
-    edges = []
-    for i in range(lines):
-        for j in range(lines):
-            if adjancy_matrix[i][j] != 0:
-                edges.append((i, j, adjancy_matrix[i][j]))
-    return edges
-
-
 def order_edges_by_greater_weight(edges):
     edges.sort(key=lambda x: x[2], reverse=True)
     return edges
+
+# Todo: implement the algorithm
+def check_cycle_in_graph(edges):
+    return False
 
 
 with open(filename, 'r') as f:
@@ -46,14 +25,12 @@ with open(filename, 'r') as f:
             lines = max(lines, int(line.split()[3]))
             vertices.append(line)
         elif line.startswith('e'):
-            fill_matrix(matrix_filled)
-            matrix_filled = True
             split_line = line.split()
             n1 = int(split_line[1])
             n2 = int(split_line[2])
             third_values.append(int(split_line[3]))
             weight = int(split_line[4])
-            adjancy_matrix[n1][n2] = weight
+            edges.append((n1, n2, weight))
 
-
-
+edges = order_edges_by_greater_weight(edges)
+print(edges)
