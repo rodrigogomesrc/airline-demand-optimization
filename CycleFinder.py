45ca17b4e355eb65e38a98a2e075bb69d245cebc
rodrigogomesrc <rocha.rodrigogomes@gmail.com>
2022-07-13 20:27:06 -0300

    fix initialization of marked vertix

--- CycleFinder.py
+++ CycleFinder.py
@@ -3,14 +3,13 @@ class CycleFinder:
     def __init__(self, edges):
         print(edges)
         self.adjacency_list = self.get_adjacency_list(edges)
-        self.print_adjacency_list()
         self.stack = []
         self.vertices_visited_info = {}
         self.cycle_exists = False
 
         for edge in edges:
-            if edge[0] not in self.vertices_visited_info:
-                self.vertices_visited_info[edge[0]] = False
+            self.vertices_visited_info[edge[0]] = False
+            self.vertices_visited_info[edge[1]] = False
            
 
     def get_adjacency_list(self, edges):
@@ -27,7 +26,6 @@ class CycleFinder:
         
         return adjacency_list
     
-   #print adjacency_list line by line
     def print_adjacency_list(self):
         for key in self.adjacency_list:
             print(key, self.adjacency_list[key])
@@ -35,8 +33,6 @@ class CycleFinder:
         
 
     def is_cycle(self):
-        print("search using...")
-        print(list(self.adjacency_list.keys())[0])
         self.search(list(self.adjacency_list.keys())[0])
         return self.is_cycle
 
@@ -45,7 +41,7 @@ class CycleFinder:
         self.vertices_visited_info[vertex] = True
         self.stack.append(vertex)
 
-        for v in    self.adjacency_list[vertex]:
+        for v in self.adjacency_list[vertex]:
             if not self.vertices_visited_info[v]:
                 self.search(v)
 
